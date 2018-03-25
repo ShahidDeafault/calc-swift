a17d67472d0e30e62432da868382c882b1820cf2
audwinoyong <audwinoyong@live.com>
2018-03-25 11:29:38 +1100

    Added error handling class

---
+++ calc/CalcError.swift
@@ -0,0 +1,13 @@
+//
+//  CalcError.swift
+//  calc
+//
+//  Created by Audwin on 25/3/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+enum CalcError : Error {
+    case divisionByZero
+}
---
+++ calc/Expression.swift
@@ -0,0 +1,15 @@
+//
+//  Expression.swift
+//  calc
+//
+//  Created by Audwin on 24/3/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+struct Expression {
+    var expression: [String]
+    
+    
+}
